a15153ab7ed96779660123c20f15013d4efe10f2
dathubt <dathalongbay@gmail.com>
2018-08-13 15:53:31 +0700

    Add files via upload

--- phpmvc/core/database/database.php
+++ phpmvc/core/database/database.php
@@ -1,18 +1,33 @@
 <?php
 class Database {
 
+    public static $connection = null;
 
     public function __construct()
     {
+        if (self::$connection) {
+            return self::$connection;
+        }
 
+        $this->connect();
     }
 
     public function connect() {
+        $servername = "localhost";
+        $username = "username";
+        $password = "password";
+        $dbname = "myDB";
 
+        // Create connection
+        self::$connection = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if (self::$connection->connect_error) {
+            die("Connection failed: " . self::$connection->connect_error);
+        }
     }
 
     public function disconnect() {
-
+        self::$connection->close();
     }
 
 }
